4c02829145da06307234ba12eaeaf0ccd1e3ac0e
cbelka <cbelka@cbmbp.localdomain>
2010-07-08 14:45:04 +0200

    added TestdataGenerator and Testcase

---
+++ src/main/java/com/anderscore/springbatch/article/domain/utils/DataGenerator.java
@@ -0,0 +1,69 @@
+package com.anderscore.springbatch.article.domain.utils;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+
+import com.anderscore.springbatch.article.domain.DetailEntity;
+import com.anderscore.springbatch.article.domain.MasterEntity;
+
+public class DataGenerator {
+	@PersistenceContext
+	private EntityManagerFactory entityManagerFactory;
+
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+
+
+	public void setEntityManagerFactory(EntityManagerFactory emf) {
+		this.entityManagerFactory = emf;
+	}
+
+
+	private String generateRandomString(int length) {
+
+			    char[] randomString = new char[length];
+			    int c  = 'A';
+			    int  r1 = 0;
+			    for (int i=0; i < length; i++)
+			    {
+			      r1 = (int)(Math.random() * 3);
+			      switch(r1) {
+			        case 0: c = '0' +  (int)(Math.random() * 10); break;
+			        case 1: c = 'a' +  (int)(Math.random() * 26); break;
+			        case 2: c = 'A' +  (int)(Math.random() * 26); break;
+			      }
+			      randomString[i] = (char)c;
+			    }
+			    return new String(randomString);
+			  }
+
+	
+	private Integer generateRandomInteger(final int length) {
+		return new Double(Math.random() * length).intValue();
+	}
+		
+	public void generateData(Integer entryCount ) {
+		EntityManager em = entityManagerFactory.createEntityManager();
+		for (int i = 0 ; i < entryCount; i++ ) {
+			MasterEntity tempMasterEntity = new MasterEntity(); 
+			BigDecimal bigDecimalField = new BigDecimal(generateRandomInteger(1000));
+			tempMasterEntity.setBigDecimalField(bigDecimalField);
+			tempMasterEntity.setDateField(new Date());
+			tempMasterEntity.setStringField(generateRandomString(10));
+			
+			DetailEntity detailEntity = new DetailEntity(); 
+			detailEntity.setDetailIdentifier(new Date());
+			
+			tempMasterEntity.setDetailEntity(detailEntity);
+			
+			em.persist(tempMasterEntity);
+			
+		}
+		
+	}
+}
---
+++ src/test/java/com/anderscore/springbatch/article/domain/utils/DataGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.anderscore.springbatch.article.domain.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+@ContextConfiguration(locations = { "classpath:/DataGeneratorContext.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
+
+public class DataGeneratorTest {
+
+	@Test
+	public void testGenerateData() {
+		DataGenerator  dataGenerator = new DataGenerator();
+		dataGenerator.generateData(20);
+	}
+
+}
